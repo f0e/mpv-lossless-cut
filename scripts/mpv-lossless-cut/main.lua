4aaaf2bd4e62a05e39484829641888ea996c0584
f0e <7321764+f0e@users.noreply.github.com>
2025-03-13 02:58:53 +1000

    feat: no cuts to clear message & format code

--- scripts/mpv-lossless-cut/main.lua
+++ scripts/mpv-lossless-cut/main.lua
@@ -1,12 +1,12 @@
-mp.msg = require 'mp.msg'
-mp.utils = require "mp.utils"
-mp.options = require 'mp.options'
+mp.msg = require("mp.msg")
+mp.utils = require("mp.utils")
+mp.options = require("mp.options")
 
 MAKE_CUTS_SCRIPT_PATH = mp.utils.join_path(mp.get_script_directory(), "make_cuts")
 
 options = {
 	output_dir = ".",
-	multi_cut_mode = "separate"
+	multi_cut_mode = "separate",
 }
 
 mp.options.read_options(options, "mpv-lossless-cut")
@@ -20,16 +20,21 @@ function log(...)
 end
 
 function cut_toggle_mode()
-	if options["multi_cut_mode"]=="separate" then options["multi_cut_mode"]="merge" else options["multi_cut_mode"]="separate" end
-	log(string.format("Cut mode set to \"%s\"", options["multi_cut_mode"]))
+	if options["multi_cut_mode"] == "separate" then
+		options["multi_cut_mode"] = "merge"
+	else
+		options["multi_cut_mode"] = "separate"
+	end
+
+	log(string.format('Cut mode set to "%s"', options["multi_cut_mode"]))
 end
 
 function cut_render()
-	if cuts[cut_key()] == nil or cuts[cut_key()]['end'] == nil then
+	if cuts[cut_key()] == nil or cuts[cut_key()]["end"] == nil then
 		log("No cuts to render")
 		return
 	end
-	
+
 	local cuts_json = mp.utils.format_json(cuts)
 	local options_json = mp.utils.format_json(options)
 
@@ -41,8 +46,7 @@ function cut_render()
 	log("Rendering...")
 	print("making cut")
 
-	local args = { "node", MAKE_CUTS_SCRIPT_PATH,
-		indir, options_json, filename, cuts_json }
+	local args = { "node", MAKE_CUTS_SCRIPT_PATH, indir, options_json, filename, cuts_json }
 
 	res, err = mp.command_native({
 		name = "subprocess",
@@ -58,11 +62,11 @@ function cut_render()
 end
 
 function cut_key()
-	return tostring(cut_index)  -- dumb, mp.utils.format_json only accepts string keys
+	return tostring(cut_index) -- dumb, mp.utils.format_json only accepts string keys
 end
 
 function cut_set_start(start_time)
-	if cuts[cut_key()] ~= nil and cuts[cut_key()]['end'] then
+	if cuts[cut_key()] ~= nil and cuts[cut_key()]["end"] then
 		cut_index = cut_index + 1
 	end
 
@@ -70,39 +74,50 @@ function cut_set_start(start_time)
 		cuts[cut_key()] = {}
 	end
 
-	cuts[cut_key()]['start'] = start_time
+	cuts[cut_key()]["start"] = start_time
 	log(string.format("[cut %d] Set start time: %.2fs", cut_index + 1, start_time))
 end
 
 function cut_set_end(end_time)
 	if cuts[cut_key()] == nil then
-		log('No start point found')
+		log("No start point found")
 		return
 	end
 
-	cuts[cut_key()]['end'] = end_time
+	cuts[cut_key()]["end"] = end_time
 	log(string.format("[cut %d] Set end time: %.2fs", cut_index + 1, end_time))
 end
 
 function cut_clear()
-	local next = next
-	if next(cuts) ~= nil then
+	if next(cuts) then
 		cuts = {}
 		cut_index = 0
-		print("Cuts cleared")
+		log("Cuts cleared")
+	else
+		log("No cuts to clear")
 	end
 end
 
-mp.add_key_binding('g', "cut_set_start", function() cut_set_start(mp.get_property_number("time-pos")) end)
-mp.add_key_binding('h', "cut_set_end", function() cut_set_end(mp.get_property_number("time-pos")) end)
+mp.add_key_binding("g", "cut_set_start", function()
+	cut_set_start(mp.get_property_number("time-pos"))
+end)
 
-mp.add_key_binding('G', "cut_set_start_sof", function() cut_set_start(0) end)
-mp.add_key_binding('H', "cut_set_end_eof", function() cut_set_end(mp.get_property('duration')) end)
+mp.add_key_binding("h", "cut_set_end", function()
+	cut_set_end(mp.get_property_number("time-pos"))
+end)
 
-mp.add_key_binding('ctrl+G', "cut_toggle_mode", cut_toggle_mode)
-mp.add_key_binding('ctrl+H', "cut_clear", cut_clear)
+mp.add_key_binding("G", "cut_set_start_sof", function()
+	cut_set_start(0)
+end)
 
-mp.add_key_binding('r', "cut_render", cut_render)
+mp.add_key_binding("H", "cut_set_end_eof", function()
+	cut_set_end(mp.get_property("duration"))
+end)
+
+mp.add_key_binding("ctrl+g", "cut_toggle_mode", cut_toggle_mode)
+mp.add_key_binding("ctrl+h", "cut_clear", cut_clear)
+
+mp.add_key_binding("r", "cut_render", cut_render)
 
 mp.register_event("end-file", cut_clear)
 
--- scripts/mpv-lossless-cut/make_cuts.js
+++ scripts/mpv-lossless-cut/make_cuts.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const child_process = require('child_process');
-const path = require('path');
+const fs = require("fs");
+const child_process = require("child_process");
+const path = require("path");
 
-const red = '\x1b[31m';
-const plain = '\x1b[0m';
-const green = '\x1b[32m';
-const purple = '\x1b[34m';
+const red = "\x1b[31m";
+const plain = "\x1b[0m";
+const green = "\x1b[32m";
+const purple = "\x1b[34m";
 
 // https://stackoverflow.com/a/45242825
 const isSubdirectory = (parent, child) => {
   const relative = path.relative(parent, child);
-  return relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+  return relative && !relative.startsWith("..") && !path.isAbsolute(relative);
 };
 
 const ffmpegEscapeFilepath = (path) =>
-  path.replaceAll('\\', '\\\\').replaceAll("'", "'\\''");
+  path.replaceAll("\\", "\\\\").replaceAll("'", "'\\''");
 
 function quit(s) {
-  console.log('' + red + s + ', quitting.' + plain + '\n');
+  console.log("" + red + s + ", quitting." + plain + "\n");
   return process.exit(1);
 }
 
@@ -39,7 +39,7 @@ function toHMS(secs) {
   if (minutes > 0) str.push(`${minutes}m`);
   if (remainingSeconds > 0) str.push(`${remainingSeconds}s`);
 
-  return str.length == 0 ? '0' : str.join('');
+  return str.length == 0 ? "0" : str.join("");
 }
 
 async function transferTimestamps(inPath, outPath, offset = 0) {
@@ -52,44 +52,44 @@ async function transferTimestamps(inPath, outPath, offset = 0) {
       mtime.getTime() / 1000 + offset
     );
   } catch (err) {
-    console.error('Failed to set output file modified time', err);
+    console.error("Failed to set output file modified time", err);
   }
 }
 
 async function ffmpeg(args) {
-  const cmd = 'ffmpeg';
+  const cmd = "ffmpeg";
   const baseArgs = [
     // hide output
-    '-nostdin',
-    '-loglevel',
-    'error',
+    "-nostdin",
+    "-loglevel",
+    "error",
     // overwrite existing files
-    '-y',
+    "-y",
   ];
 
   const fullArgs = baseArgs.concat(args);
 
-  const cmdStr = '' + cmd + ' ' + fullArgs.join(' ');
-  console.log('' + purple + cmdStr + plain + '\n');
+  const cmdStr = "" + cmd + " " + fullArgs.join(" ");
+  console.log("" + purple + cmdStr + plain + "\n");
 
-  child_process.spawnSync(cmd, fullArgs, { stdio: 'inherit' });
+  child_process.spawnSync(cmd, fullArgs, { stdio: "inherit" });
 }
 
 async function renderCut(inpath, outpath, start, duration) {
   const args = [
     // seek to start before loading file (faster) https://trac.ffmpeg.org/wiki/Seeking#Inputseeking
-    '-ss',
+    "-ss",
     start,
-    '-t',
+    "-t",
     duration,
-    '-i',
+    "-i",
     inpath,
     // don't re-encode
-    '-c',
-    'copy',
+    "-c",
+    "copy",
     // shift timestamps so they start at 0
-    '-avoid_negative_ts',
-    'make_zero',
+    "-avoid_negative_ts",
+    "make_zero",
     outpath,
   ];
 
@@ -100,21 +100,21 @@ async function mergeCuts(tempPath, filepaths, outpath) {
   // i hate that you have to do a separate command and render each cut separately first, i tried using
   // filter_complex for merging with multiple inputs but it wouldn't let me. todo: look into this further
 
-  const mergeFile = path.join(tempPath, 'merging.txt');
+  const mergeFile = path.join(tempPath, "merging.txt");
   await fs.promises.writeFile(
     mergeFile,
-    filepaths.map((path) => `file '${ffmpegEscapeFilepath(path)}`).join('\n')
+    filepaths.map((path) => `file '${ffmpegEscapeFilepath(path)}`).join("\n")
   );
 
   await ffmpeg([
-    '-f',
-    'concat',
-    '-safe',
+    "-f",
+    "concat",
+    "-safe",
     0,
-    '-i',
+    "-i",
     mergeFile,
-    '-c',
-    'copy',
+    "-c",
+    "copy",
     outpath,
   ]);
 
@@ -130,13 +130,13 @@ async function main() {
 
   const [indir, optionsStr, filename, cutsStr] = argv;
 
-  if (!isDir(indir)) quit('Input directory is invalid');
+  if (!isDir(indir)) quit("Input directory is invalid");
 
   const options = JSON.parse(optionsStr);
   const outdir = path.resolve(indir, options.output_dir);
 
   if (!isDir(outdir)) {
-    if (!isSubdirectory(indir, outdir)) quit('Output directory is invalid');
+    if (!isSubdirectory(indir, outdir)) quit("Output directory is invalid");
 
     // the output directory is a child of the input directory, can assume it's safe to create
     await fs.promises.mkdir(outdir, { recursive: true });
@@ -151,28 +151,28 @@ async function main() {
   const outpaths = [];
 
   for (const [i, cut] of cuts.entries()) {
-    if (!('end' in cut)) continue;
+    if (!("end" in cut)) continue;
 
     const duration = parseFloat(cut.end) - parseFloat(cut.start);
 
     const cutName =
-      `(cut${cuts.length == 1 ? '' : i + 1}) ` +
+      `(cut${cuts.length == 1 ? "" : i + 1}) ` +
       filename_noext +
-      ' (' +
+      " (" +
       toHMS(cut.start) +
-      ' - ' +
+      " - " +
       toHMS(cut.end) +
-      ')' +
+      ")" +
       ext;
 
     const outpath = path.join(outdir, cutName);
 
-    const progress = '(' + (i + 1) + '/' + cuts.length + ')';
+    const progress = "(" + (i + 1) + "/" + cuts.length + ")";
 
     console.log(
-      '' + green + progress + plain + ' ' + inpath + ' ' + green + '->' + plain
+      "" + green + progress + plain + " " + inpath + " " + green + "->" + plain
     );
-    console.log('' + outpath + '\n');
+    console.log("" + outpath + "\n");
 
     await renderCut(inpath, outpath, cut.start, duration);
     await transferTimestamps(inpath, outpath);
@@ -180,7 +180,7 @@ async function main() {
     outpaths.push(outpath);
   }
 
-  if (outpaths.length > 1 && options.multi_cut_mode == 'merge') {
+  if (outpaths.length > 1 && options.multi_cut_mode == "merge") {
     const cutName = `(${outpaths.length} merged cuts) ` + filename;
     const outpath = path.join(outdir, cutName);
 
@@ -188,7 +188,7 @@ async function main() {
     await transferTimestamps(inpath, outpath);
   }
 
-  return console.log('Done.\n');
+  return console.log("Done.\n");
 }
 
 main();
