3d2a4f95d9177e13f1a0f93b3cbb68fc533add2a
Myphz <danielkeq@gmail.com>
2025-06-30 20:26:28 +0400

    fix: copy only video & audio stream

--- mpv-lossless-cut.lua
+++ mpv-lossless-cut.lua
@@ -171,9 +171,12 @@ local function render_cut(input, outpath, start, duration, input_mtime)
 		tostring(duration),
 		"-i",
 		input,
-		-- copy all input streams
+		-- copy video stream
 		"-map",
-		"0",
+		"0:v",
+		-- copy audio stream if present
+		"-map",
+		"0:a?",
 		-- shift timestamps so they start at 0
 		"-avoid_negative_ts",
 		"make_zero",
@@ -203,7 +206,7 @@ local function merge_cuts(temp_dir, filepaths, outpath, input_mtime)
 	local content = ""
 
 	for _, path in ipairs(filepaths) do
-			content = content .. string.format("file '%s'\n", ffmpeg_escape_filepath(path))
+		content = content .. string.format("file '%s'\n", ffmpeg_escape_filepath(path))
 	end
 
 	local file = io.open(merge_file, "w")
@@ -286,9 +289,9 @@ local function cut_render()
 	local cwd = mp.utils.getcwd()
 	local outdir
 	if options.output_dir == "." then
-			outdir = cwd
+		outdir = cwd
 	else
-			outdir = mp.utils.join_path(cwd, options.output_dir)
+		outdir = mp.utils.join_path(cwd, options.output_dir)
 	end
 
 	-- create output directory if needed
